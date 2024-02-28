4b2209b5a769d11fbab6faa413022f5bf847e0ef
Sezgin <112806549+sezginaliunal@users.noreply.github.com>
2024-02-28 14:04:53 +0300

    Update auth_service.dart

--- lib/app/data/services/auth_service.dart
+++ lib/app/data/services/auth_service.dart
@@ -1,11 +1,10 @@
 import 'package:http/http.dart' as http;
-import 'package:started_app/app/data/services/api/api_response.dart';
+import 'package:cingo/app/data/services/api/api_response.dart';
 import 'dart:convert';
 
-import 'package:started_app/app/data/services/api/base_api_service.dart';
-import 'package:started_app/app/data/services/api/custom_header.dart';
-import 'package:started_app/app/res/constants/app_urls.dart';
-import 'package:started_app/app/res/utils/snackBar_helper.dart';
+import 'package:cingo/app/data/services/api/base_api_service.dart';
+import 'package:cingo/app/data/services/api/custom_header.dart';
+import 'package:cingo/app/res/constants/app_urls.dart';
 
 class AuthService implements BaseApiService {
   @override
@@ -17,41 +16,21 @@ class AuthService implements BaseApiService {
       Uri.parse('$baseApiUrl/$endpoint'),
       headers: CustomHeaders.create(),
     );
-    if (response.statusCode == 201) {
-      SnackbarHelper.showSuccessSnackbar('Success', 'Congratulations');
-      return ApiResponse(
-          success: true, data: jsonDecode(response.body), errorMessage: '');
-    } else {
-      final data = jsonDecode(response.body);
-      SnackbarHelper.showErrorSnackbar('Error', data['error']);
-      return ApiResponse(
-        success: false,
-        errorMessage: 'HTTP Error: ${response.statusCode}',
-        data: data,
-      );
-    }
+
+    return ApiResponse.fromHttpResponse(response);
   }
 
   @override
   Future<ApiResponse<Map<String, dynamic>>> post(
-      String endpoint, Map<String, dynamic> body) async {
+    String endpoint,
+    Map<String, dynamic> body,
+  ) async {
     final response = await http.post(
       Uri.parse('$baseApiUrl/$endpoint'),
       headers: CustomHeaders.create(),
       body: jsonEncode(body),
     );
-    if (response.statusCode == 201) {
-      SnackbarHelper.showSuccessSnackbar('Success', 'Congratulations');
-      return ApiResponse(
-          success: true, data: jsonDecode(response.body), errorMessage: '');
-    } else {
-      final data = jsonDecode(response.body);
-      SnackbarHelper.showErrorSnackbar('Error', data['message']);
-      return ApiResponse(
-        success: false,
-        errorMessage: 'HTTP Error: ${response.statusCode}',
-        data: data,
-      );
-    }
+
+    return ApiResponse.fromHttpResponse(response);
   }
 }
