e30d88cdc92af92e4ddb8b5dd51be389cd1787e9
Sezgin <112806549+sezginaliunal@users.noreply.github.com>
2024-02-28 14:03:44 +0300

    Update custom_text_form.dart

--- lib/app/res/widget/custom_text_form.dart
+++ lib/app/res/widget/custom_text_form.dart
@@ -1,29 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class CustomTextFormField extends StatelessWidget {
+class CustomTextFormField extends StatefulWidget {
+  const CustomTextFormField(
+      {Key? key,
+      required this.obSecureText,
+      required this.controller,
+      required this.hintText,
+      this.validator})
+      : super(key: key);
+
+  final bool obSecureText;
   final TextEditingController controller;
   final String hintText;
   final String Function(String?)? validator;
 
-  const CustomTextFormField({
-    Key? key,
-    required this.controller,
-    required this.hintText,
-    this.validator,
-  }) : super(key: key);
+  // Method to toggle obscure text
+  void toggleObsecure() => _CustomTextFormFieldState()._toggleObsecure();
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  bool _obSecureText = false;
+
+  // Method to toggle obscure text
+  void _toggleObsecure() {
+    setState(() {
+      _obSecureText = !_obSecureText;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _obSecureText =
+        widget.obSecureText; // Set initial state based on obSecureText prop
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-        controller: controller,
-        decoration: InputDecoration(
-          hintText: hintText,
-        ),
-        validator: (value) {
-          if (value == null || value.isEmpty || value.trim() == '') {
-            return '$hintText is required';
-          }
-          return null;
-        });
+      style: Get.textTheme.titleSmall,
+      controller: widget.controller,
+      obscureText: widget.obSecureText ? _obSecureText : false,
+      validator: (value) {
+        if (value == null || value.isEmpty || value.trim() == '') {
+          return '${widget.hintText} is required';
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        hintText: widget.hintText,
+        suffixIcon: widget.obSecureText
+            ? IconButton(
+                onPressed: _toggleObsecure,
+                icon: Icon(
+                    _obSecureText ? Icons.visibility : Icons.visibility_off),
+              )
+            : null, // If it's not a password field, don't show suffixIcon
+      ),
+    );
   }
 }
