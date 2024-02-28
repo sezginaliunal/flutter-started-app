f21c5947b10ca81be8458fc9e63d7b094ecf64cf
Sezgin <112806549+sezginaliunal@users.noreply.github.com>
2024-02-28 19:49:48 +0300

    Update register.dart

--- lib/app/pages/auth/register.dart
+++ lib/app/pages/auth/register.dart
@@ -1,7 +1,11 @@
+import 'package:cingo/app/res/boxes/height.dart';
+import 'package:cingo/app/res/constants/paddings.dart';
+import 'package:cingo/app/res/widget/auth_button.dart';
+import 'package:cingo/app/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:started_app/app/controllers/auth_controller.dart';
-import 'package:started_app/app/res/widget/custom_text_form.dart';
+import 'package:cingo/app/controllers/auth_controller.dart';
+import 'package:cingo/app/res/widget/custom_text_form.dart';
 
 class RegisterPage extends GetView<AuthController> {
   const RegisterPage({Key? key}) : super(key: key);
@@ -10,39 +14,79 @@ class RegisterPage extends GetView<AuthController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Form(
-          key: controller.mainController.formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CustomTextFormField(
-                controller: controller.email.value,
-                hintText: 'Email',
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: getPaddingValue(CustomPadding.medium),
+            child: Form(
+              key: controller.mainController.registerFormKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.person,
+                    size: 80,
+                  ),
+                  const HeightBoxesItems.heightTen(),
+                  Text('Welcome back to us!', style: Get.textTheme.titleSmall),
+                  const HeightBoxesItems.heightTen(),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      CustomTextFormField(
+                        controller: controller.email.value,
+                        hintText: 'Email',
+                        obSecureText: false,
+                      ),
+                      const HeightBoxesItems.heightFive(),
+                      CustomTextFormField(
+                        controller: controller.password.value,
+                        hintText: 'Password',
+                        obSecureText: true,
+                      ),
+                      const HeightBoxesItems.heightFive(),
+                      CustomTextFormField(
+                        obSecureText: false,
+                        controller: controller.name.value,
+                        hintText: 'Name',
+                      ),
+                      const HeightBoxesItems.heightFive(),
+                      CustomTextFormField(
+                        obSecureText: false,
+                        controller: controller.lastName.value,
+                        hintText: 'Last Name',
+                      ),
+                      const HeightBoxesItems.heightFive(),
+                    ],
+                  ),
+                  const HeightBoxesItems.heightTen(),
+                  Obx(() => SizedBox(
+                        width: Get.width,
+                        child: controller.mainController.loading.isTrue
+                            ? const Center(
+                                child: CircularProgressIndicator.adaptive())
+                            : AuthButton(
+                                onPressed:
+                                    controller.mainController.loading.isTrue
+                                        ? null
+                                        : () {
+                                            if (controller.mainController
+                                                .registerFormKey.currentState!
+                                                .validate()) {
+                                              controller.register();
+                                            }
+                                          },
+                                text: 'Sign up',
+                              ),
+                      )),
+                  TextButton(
+                      onPressed: () => Get.offAllNamed(AppRoutes.LOGIN),
+                      child:
+                          Text('Login now!', style: Get.textTheme.titleSmall))
+                ],
               ),
-              CustomTextFormField(
-                controller: controller.password.value,
-                hintText: 'Password',
-              ),
-              CustomTextFormField(
-                controller: controller.lastName.value,
-                hintText: 'Last Name',
-              ),
-              CustomTextFormField(
-                controller: controller.name.value,
-                hintText: 'Name',
-              ),
-              Obx(() => ElevatedButton(
-                  onPressed: controller.mainController.loading.isTrue
-                      ? null
-                      : () {
-                          if (controller.mainController.formKey.currentState!
-                              .validate()) {
-                            controller.register();
-                          }
-                        },
-                  child: const Text('Register')))
-            ],
+            ),
           ),
         ),
       ),
