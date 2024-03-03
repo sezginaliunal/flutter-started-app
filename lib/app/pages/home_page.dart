a64b49d74cda779e5ce9dfd595d1667cbd4654a1
Sezgin <sezginaliunal@hotmail.com>
2024-03-03 19:03:02 +0300

    update home_view

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:started_app/app/controllers/home_controller.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends GetView<HomeController> {
   const HomePage({Key? key}) : super(key: key);
 
   @override
