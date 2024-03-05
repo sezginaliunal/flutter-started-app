3f6755ba0a223851110effd0547038fc7b8f0765
Sezgin <sezginaliunal@hotmail.com>
2024-03-05 21:05:26 +0300

    Updated

---
+++ lib/app/res/extensions/text_editing_validate.dart
@@ -0,0 +1,15 @@
+
+extension TextEditingControllerErrorExtension on TextEditingControllerError {
+  String get errorMessage {
+    switch (this) {
+      case TextEditingControllerError.INVALID_EMAIL:
+        return 'Geçersiz email formatı.';
+      case TextEditingControllerError.EMPTY_EMAIL:
+        return 'Lütfen bir email girin.';
+      case TextEditingControllerError.INVALID_PASSWORD:
+        return 'Şifre en az 8 karakter olmalı ve en az bir büyük harf, bir küçük harf ve bir rakam içermelidir.';
+      case TextEditingControllerError.EMPTY_PASSWORD:
+        return 'Lütfen bir şifre girin.';
+    }
+  }
+}
---
+++ lib/app/res/widgets/text_form_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomFormWidget extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+  final String autofillHints;
+  const CustomFormWidget({
+    Key? key,
+    required this.controller,
+    required this.labelText,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.validator,
+    required this.autofillHints,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autofillHints: [autofillHints],
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: labelText,
+        border: const OutlineInputBorder(),
+      ),
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      validator: validator,
+    );
+  }
+}
