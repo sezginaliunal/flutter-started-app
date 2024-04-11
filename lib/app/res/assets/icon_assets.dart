4233f0ca458952e7e4cb039e4e51aa324e95b270
Your Name <your_email@example.com>
2024-04-11 20:02:56 +0300

    updated

--- lib/app/res/assets/icon_assets.dart
+++ lib/app/res/assets/icon_assets.dart
@@ -1,12 +1,6 @@
 enum IconItems {
   rosary,
-  book,
-  calendar,
-  esmaalhusna,
-  prayer,
-  qibla,
-  radio,
-  settings
+
 }
 
 extension IconItemsExtension on IconItems {
@@ -14,20 +8,7 @@ extension IconItemsExtension on IconItems {
     switch (this) {
       case IconItems.rosary:
         return 'rosary';
-      case IconItems.book:
-        return 'book';
-      case IconItems.calendar:
-        return 'calendar';
-      case IconItems.esmaalhusna:
-        return 'esmaalhusna';
-      case IconItems.prayer:
-        return 'prayer';
-      case IconItems.qibla:
-        return 'qibla';
-      case IconItems.radio:
-        return 'radio';
-      case IconItems.settings:
-        return 'settings';
+
     }
   }
 
