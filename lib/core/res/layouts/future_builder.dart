77e0fdca47e604d4e687189066618e367f4cbda4
Sezgin <112806549+sezginaliunal@users.noreply.github.com>
2024-05-10 15:29:51 +0300

    Update future_builder.dart

--- lib/core/res/layouts/future_builder.dart
+++ lib/core/res/layouts/future_builder.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 class FutureBuilderLayout<T> extends StatelessWidget {
-  const FutureBuilderLayout(
-      {Key? key,
-      required this.future,
-      required this.hasSnapshot,
-      required this.waiting})
-      : super(key: key);
+  const FutureBuilderLayout({
+    Key? key,
+    required this.future,
+    required this.hasSnapshotBuilder, // Değişiklik: hasSnapshotBuilder olarak değiştirildi
+    required this.waiting,
+  }) : super(key: key);
 
   final Future<T> future;
-  final Widget hasSnapshot;
   final Widget waiting;
+  final Widget Function(T data)
+      hasSnapshotBuilder; // Değişiklik: hasSnapshotBuilder tipi güncellendi
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<T>(
@@ -23,7 +24,8 @@ class FutureBuilderLayout<T> extends StatelessWidget {
             child: Text('Error: ${snapshot.error}'),
           );
         } else {
-          return hasSnapshot;
+          // Değişiklik: hasSnapshotBuilder kullanılarak snapshot verisine göre widget oluşturuluyor
+          return hasSnapshotBuilder(snapshot.data as T);
         }
       },
     );
