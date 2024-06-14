7769fb3a573d36085ec3c70c7c1d7297e0c240c9
Your Name <your_email@example.com>
2024-06-14 13:42:20 +0300

    updated

--- lib/core/services/api/base_network.dart
+++ lib/core/services/api/base_network.dart
@@ -4,8 +4,12 @@ abstract class BaseApiService<T> {
   late final INetworkManager networkManager;
 
   BaseApiService(String baseUrl) {
-    final options = BaseOptions(baseUrl: baseUrl);
-    networkManager = NetworkManager<EmptyModel>(options: options);
+    final options = BaseOptions(
+      baseUrl: baseUrl,
+    );
+    networkManager = NetworkManager<EmptyModel>(
+      options: options,
+    );
   }
 
   Future<IResponseModel<T?, INetworkModel?>?> getData(String endpoint);
